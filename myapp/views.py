68962320e86156ba350bc4ad55fcdc90f2927036
Maulana <fendy.maulana@accenture.com>
2025-01-31 15:09:29 +0700

    push

--- myapp/views.py
+++ myapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import generics
+from rest_framework.response import Response 
 
 # Create your views here.
 
@@ -9,6 +10,12 @@ from rest_framework import generics
 class BookListCreateView(generics.ListCreateAPIView):
       queryset = Book.objects.all()
       serializer_class = BookSerializer
+
+      def list(self, request, *args, **kwargs):
+            queryset = self.get_queryset()
+            serialize = self.get_serializer(queryset,many=True)
+            return Response({"data": serialize.data})
+
 
 #Create Retrive, Update, Delete Book
 class BookRetriveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
